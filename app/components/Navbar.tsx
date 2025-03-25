ebbf2b8de0d05dec51755f3ae9e403841458aef8
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-25 05:10:16 +0100

    feat: navbar login auth working

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link"
+import Image from "next/image"
+import {auth, signOut, signIn} from '@/auth'
+
+const Navbar = async () => {
+    const session = await auth();
+  return (
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+            <Link href='/'>
+                <Image src='/yc-logo.svg' alt='logo' width={144} height={30}/>
+            </Link>
+            <div className="flex items-center gap-5">
+                {(session && session?.user) ? (
+                    <>
+                        <Link href="/startup/create">
+                            <span>Create</span>
+                        </Link>
+                        <button type="submit" onClick={signOut}>
+                            <span>Logout</span>
+                        </button>
+
+                        <Link href={`/user/${session?.id}`}>
+                            <span>{session?.user?.name}</span>
+                        </Link>
+                    </>
+                ): (
+                    <form action={async () => {
+                        "use server";
+
+                        await signIn('github')
+                    }}>
+                        <button type="submit">
+                            Login
+                        </button>
+
+                    </form>                
+                )}
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
