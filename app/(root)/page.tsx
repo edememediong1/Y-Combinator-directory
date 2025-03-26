cd9187e1271f134cd5930d913b09fe2fb56a264a
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-27 00:36:45 +0100

    background structure of the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
       <main className="">
-          <h1 className="heading">Hello</h1>
+        <section className="pink_container">
+          <h1 className="heading">Pitch your startup<br/> Connect with entreprenuers</h1>
+          <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
+        </section>
+          <SearchForm/>
       </main>  
   );
 }
