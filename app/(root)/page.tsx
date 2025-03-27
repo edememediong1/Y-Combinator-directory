3ec654c829939755930dc5dea24b87d477475b1d
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-27 01:33:59 +0100

    added reset button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
         <section className="pink_container">
           <h1 className="heading">Pitch your startup<br/> Connect with entreprenuers</h1>
           <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
-        </section>
           <SearchForm/>
+        </section>
+          
       </main>  
   );
 }
--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
 
 const SearchForm = () => {
+    const query = 'Test'
+
+    
   return (
-    <div>SearchForm</div>
+    <Form action='/' scroll={false} className="search-form">
+        <input 
+            name="query"
+            defaultValue=""
+            className='search-input'
+            placeholder='Search Startups'
+        />
+
+        <div className='flex gap-2'>
+            {query && (
+               <SearchFormReset/>
+            )}
+        </div>
+    </Form>
   )
 }
 
