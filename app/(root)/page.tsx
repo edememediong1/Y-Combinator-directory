35d5f9cfe8fc9c0284519008f5248bd8c7d2192d
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-27 14:29:09 +0100

    feat: added dynamic Startup Card headers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,10 +7,18 @@ export default async function Home({searchParams}: {
   const query = (await searchParams).query
   return (
       <main className="">
+        {/* Hero Section */}
         <section className="pink_container">
           <h1 className="heading">Pitch your startup<br/> Connect with entreprenuers</h1>
           <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
           <SearchForm query={query}/>
+        </section>
+
+        {/* Startup Cards */}
+        <section className="section_container">
+          <p className="text-30-semibold">
+            {query ? `Search results for "${query}" ` : "All Startups"}
+          </p>
         </section>
           
       </main>  
