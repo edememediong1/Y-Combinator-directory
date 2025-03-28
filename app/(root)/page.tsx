fff4a2d8fc6af53d51fea08e483230e5afa7de91
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-28 23:55:47 +0100

    fix: test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,17 @@ export default async function Home({searchParams}: {
 }) {
 
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt : 'Yesterday',
+    views: 55,
+    author : {_id: 1},
+    _id: 1,
+    description: 'This is a description.',
+    image: 'https://unsplash.com/photos/steamed-buns-in-a-bamboo-steamer-c3U4xmOSWc8',
+    category: 'Robots',
+    title: "Xylem"
+  }]
   return (
       <main className="">
         {/* Hero Section */}
@@ -19,6 +30,12 @@ export default async function Home({searchParams}: {
           <p className="text-30-semibold">
             {query ? `Search results for "${query}" ` : "All Startups"}
           </p>
+
+          <ul className="mt-7 card_grid">
+              {posts?.length > 0 ? (
+                posts.map((post: StartupCardType, index: number) =>)
+              )}
+          </ul>
         </section>
           
       </main>  
